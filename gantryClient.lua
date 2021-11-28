3b116309fef276ade16936bda23f03987357ab18
SYPIAC <Reks888@hotmail.com>
2021-11-28 12:13:55 +0100

    Added movement in all directions

--- gantryClient.lua
+++ gantryClient.lua
@@ -1,5 +1,53 @@
+--redstone sides
+side_shift   = "right"
+side_gantryX = "left"
+side_gantryZ = "back"
+side_clutch = "front"
+top = "top"
+
 local port = 1
 local modem = peripheral.wrap("top")
+
+--helper function for redstone
+function reds(side, bool)
+  redstone.setOutput(side, bool)
+end
+
+function processCmd(cmd)
+  -- turn on clutch to not break anything
+  reds(side_clutch, true)
+  os.sleep(0.2)
+  if(cmd=="up") then
+    reds(side_gantryX, true)
+    reds(side_gantryZ, true)
+    reds(side_shift, true)
+  elseif(cmd=="down") then
+    reds(side_gantryX, true)
+    reds(side_gantryZ, true)
+    reds(side_shift, false)
+  elseif(cmd=="left") then
+    reds(side_gantryX, true)
+    reds(side_gantryZ, false)
+    reds(side_shift, true)
+  elseif(cmd=="right") then
+    reds(side_gantryX, true)
+    reds(side_gantryZ, false)
+    reds(side_shift, false)
+  elseif(cmd=="forward") then
+    reds(side_gantryX, false)
+    reds(side_gantryZ, false)
+    reds(side_shift, false)
+  elseif(cmd=="back") then
+    reds(side_gantryX, false)
+    reds(side_gantryZ, false)
+    reds(side_shift, true)
+  end
+  -- resume movement
+  reds(side_clutch, false)
+  os.sleep(0.2)
+end
+
+--main begins here
 if not modem.isWireless then
   print("NO top modem found!")
   print("Terminating")
@@ -10,17 +58,13 @@ modem.open(port)
 while(true) do
   local event, modemSide, senderChannel,
         replyChannel, message, senderDistance = os.pullEvent("modem_message")   
-  print("Received message:"..message)
-  if(string.find(message, "gantry")) then
-    --cut out the "gantry:" prefix and process the rest
+  print("Received message "..message)
+  if(string.find(message, "quarry")) then
+    --cut out the "quarry:" prefix and process the rest
     processCmd(string.sub(message, 8, 99))
   end
 end
 
-function processCmd(cmd)
-  if(cmd=="up") then
-    redstone.setOutput("back", true)
-  elseif(cmd=="down") then
-    redstone.setOutput("back", false)
-  end
+function processOSEvent(event)
+
 end
--- gantryServer.lua
+++ gantryServer.lua
@@ -1,24 +1,36 @@
 local port = 1
-local keyUp, keyDown = 265, 264
-local modem = peripheral.warp("back")
+local keyZ, keyX     = 90, 88
+local keyArrowUp, keyArrowDown, keyArrowLeft, keyArrowRight = 265, 264, 263, 262
+local modem = peripheral.wrap("back")
 
+function processKey(key)
+  if(key == keyArrowUp) then
+    modem.transmit(port,port, "quarry:forward")
+  elseif(key==keyArrowDown) then
+    modem.transmit(port,port, "quarry:back")
+  elseif(key==keyArrowLeft) then
+    modem.transmit(port,port, "quarry:left")
+  elseif(key==keyArrowRight) then
+    modem.transmit(port,port, "quarry:right")
+  elseif(key==keyZ) then
+    modem.transmit(port,port, "quarry:down")
+  elseif(key==keyX) then
+    modem.transmit(port,prot, "quarry:up")
+  end
+end
+
+--main starts here
 if not modem.isWireless then
   print("No Modem!")
   return
 end
 print("Quarry controller")
-print("Up arrow to go up, down to go down!")
+print("Z to go up, X to go down!")
+print("arrow keys to move")
 while(true) do
   local event, key, isHeld = os.pullEvent("key")
   -- do not repeat key event
   if(isHeld == false) then
-    if(key == keyUp) then
-	  print("Going up")
-	  modem.transmit(port,port, "quarry:up")
-	end
-	if(key == keyDown) then
-	  print("Going down")
-	  modem.transmit(port,port, "quarry:down")
-	end
+    processKey(key)
   end
 end
